8721496cc04ca9c15b9d94b1b3cdea9397f61527
Im Genius <im.genius.man@gmail.com>
2014-06-18 17:16:44 +0500

    bug fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,9 +23,9 @@ class ProjectsController < ApplicationController
 
 		# @project = user_client.build_projects(params_project)
 		if @project.save
-			report = @project.reports.first 
-			report.name = "#{@project.name} Report #{Date.today.to_s}"
-			report.save!
+			# report = @project.reports.first 
+			# report.name = "#{@project.name} Report #{Date.today.to_s}"
+			# report.save!
 			if current_user.role == "client"
 				users = params[:project][:userid] if params[:project][:userid].present?
 				users && users.each do |user|
